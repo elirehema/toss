8a4108c2d58369db78343454a8d5d48c8276d6f7
elirehema <rocksandystone@gmail.com>
2022-08-29 00:04:41 +0300

    Add cookie Nuxt

--- nuxt.config.js
+++ nuxt.config.js
@@ -54,7 +54,9 @@ export default {
     // https://go.nuxtjs.dev/axios
     '@nuxtjs/axios',
     // https://go.nuxtjs.dev/pwa
-    '@nuxtjs/pwa'
+    '@nuxtjs/pwa',
+
+    ['cookie-universal-nuxt', { parseJSON: false }],
   ],
 
   // Axios module configuration: https://go.nuxtjs.dev/config-axios
